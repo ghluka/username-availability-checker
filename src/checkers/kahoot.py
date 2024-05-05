6e8c36f579073178755b7d20d93d6b67c0a06301
Лука <164436043+ghluka@users.noreply.github.com>
2024-05-04 20:14:10 -0400

    Kahoot.it

---
+++ src/checkers/kahoot.py
@@ -0,0 +1,25 @@
+"""https://kahoot.it/
+"""
+import time
+
+import httpx
+from httpx._models import Response
+
+from base.checker import BaseChecker
+
+
+class Checker(BaseChecker):
+    ENDPOINT = "https://create.kahoot.it/rest/users/usernameavailable"
+
+    @BaseChecker.check.register
+    def _(self, username:str) -> str|None:
+        headers={'x-kahoot-user-identifier': username}
+
+        r = Response(429)
+        while r.status_code == 429:
+            with httpx.Client(verify=False, proxies=self.proxies) as client:
+                r = client.get(self.ENDPOINT, headers=headers)
+            if r.status_code == 429:
+                time.sleep(self.RATELIMIT_TIMEOUT)
+        
+        return username if r.json().get('isUsernameAvailable') else None
