3e8fe19e335bf095ab55aed0faad3f375c984544
Лука <164436043+ghluka@users.noreply.github.com>
2024-05-04 20:14:41 -0400

    Roblox

---
+++ src/checkers/roblox.py
@@ -0,0 +1,23 @@
+"""https://roblox.com/
+"""
+import time
+
+import httpx
+from httpx._models import Response
+
+from base.checker import BaseChecker
+
+
+class Checker(BaseChecker):
+    ENDPOINT = "https://auth.roblox.com/v1/usernames/validate?birthday=2000-01-01T00:00:00.000Z&context=Signup&username="
+
+    @BaseChecker.check.register
+    def _(self, username:str) -> str|None:        
+        r = Response(429)
+        while r.status_code == 429:
+            with httpx.Client(verify=False, proxies=self.proxies) as client:
+                r = client.get(f"{self.ENDPOINT}{username}")
+            if r.status_code == 429:
+                time.sleep(self.RATELIMIT_TIMEOUT)
+        
+        return username if r.json()['code'] == 0 else None
