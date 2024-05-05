2ca76a9c7638f8cd5ccc645db79fe5e8740c93d6
Лука <164436043+ghluka@users.noreply.github.com>
2024-05-04 21:22:44 -0400

    GitHub

---
+++ src/checkers/github.py
@@ -0,0 +1,30 @@
+"""https://github.com/
+"""
+import time
+
+import httpx
+from httpx._models import Response
+
+from base.checker import BaseChecker
+
+
+class Checker(BaseChecker):
+    ENDPOINT = "https://github.com/"
+
+    @BaseChecker.check.register
+    def _(self, username:str) -> str|None:
+        if len(username) > 39:
+            return False
+        elif username.endswith("-") or username.endswith("-") or "--" in username:
+            return False
+        elif not all(c.isalnum() or c in "-" for c in username):
+            return False
+        
+        r = Response(429)
+        while r.status_code == 429:
+            with httpx.Client(verify=False, proxies=self.proxies) as client:
+                r = client.get(f"{self.ENDPOINT}{username}")
+            if r.status_code == 429:
+                time.sleep(self.RATELIMIT_TIMEOUT)
+        
+        return username if r.status_code == 404 else None
